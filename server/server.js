47f3eda164c7b1894fef6b582e59e12e908810b7
unknown <xeniamae.abano@ssu.edu.ph>
2021-12-21 10:38:58 +0800

    gana na pls

--- server/server.js
+++ server/server.js
@@ -183,9 +183,9 @@ app.prepare().then(async () => {
     ctx.body = data;
   })
 
-  router.get('/cancelSubscription', koaBody(), verifyRequest(), async(ctx) => {
-    const data = ctx.request.body
-    const id = data.id
+  router.delete('/cancelSubscription', koaBody(), verifyRequest(), async(ctx) => {
+    const requestBody = ctx.request.body
+    const id = requestBody.id
     const session = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res);
     const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
     const data = await client.delete({
