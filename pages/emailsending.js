191375a466e9d3e51f898f2f7265477391c686c2
unknown <xeniamae.abano@ssu.edu.ph>
2021-12-21 11:07:34 +0800

    a

--- pages/emailsending.js
+++ pages/emailsending.js
@@ -98,12 +98,13 @@ const emailsending = ({authAxios}) => {
   }
 
   const handleCancelSubscription = async () => {
+    console.log('am clicked!')
       authAxios.delete('/cancelSubscription', {
         id: subscription[0].id
       })
       .then(res => {
         window.parent.location.href = res.data;
-        console.log('am clicked!')
+        
       })
   }
 
@@ -114,7 +115,7 @@ const emailsending = ({authAxios}) => {
         primaryAction={
           {
             content: 'Cancel Subscription',
-            onMouseEnter: () => handleCancelSubscription
+            onAction: () => {handleCancelSubscription}
           }
         }
         >
