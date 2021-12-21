2d70f2bb8e8f87748254df50ad8358add2b12399
unknown <xeniamae.abano@ssu.edu.ph>
2021-12-21 13:18:19 +0800

    a

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Subscription from "./subscription";
 const Index = ({authAxios}) => {
 
   return (
-        <EmailSending authAxios={authAxios}/>
-        // <Subscription authAxios={authAxios}/>
+        // <EmailSending authAxios={authAxios}/>
+        <Subscription authAxios={authAxios}/>
   )
 }
 export default Index;
